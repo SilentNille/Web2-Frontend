49ceb4b0a58767517d84b62504d3c279674b6eec
Philip Niclas <philipp.niclas@unitb.de>
2025-06-09 16:17:14 +0200

    Add a landing page that even the Avengers would be proud of!
    Files: [src/layout/styles/LandingPage.css, src/layout/img/react.svg]

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,25 +1,28 @@
-import "../styles/LandingPage.css";
+import React from "react";
+import "../layout/styles/LandingPage.css";
 
-function LandingPage() {
+class LandingPage extends React.Component {
 
-    const handleOpenLoginDialog = () => {
+    handleOpenLoginDialog = () => {
         console.log("Login dialog opened");
     };
 
-    const handleCloseLoginDialog = () => {
+    handleCloseLoginDialog = () => {
         console.log("Login dialog closed");
     };
 
-    return (
-        <div className="landing-page-container" id="LandingPage">
-            <h1>Landing Page!</h1>
-            <p>This is a simple React application built with Vite.</p>
+    render() {
+        return (
+            <div className="landing-page-container" id="LandingPage">
+                <h1>Landing Page!</h1>
+                <p>This is a simple React application built with Vite.</p>
 
-            <button className="login-button" onClick={handleOpenLoginDialog}>
-                Open Login Dialog
-            </button>
-        </div>
-    );
+                <button className="login-button" onClick={this.handleOpenLoginDialog}>
+                    Open Login Dialog
+                </button>
+            </div>
+        );
+    }
 }
 
 export default LandingPage;
