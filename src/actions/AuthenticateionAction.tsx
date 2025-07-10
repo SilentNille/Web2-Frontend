7cd367f29c608961818531f46ed3805df60ccbcc
Philip Niclas <philipp.niclas@unitb.de>
2025-07-10 20:25:54 +0200

    Delete the past like Thanos with a snap, it's gone! No more login dialog or SVG drama!
    Files: [src/actions/AuthenticateionAction.tsx, src/layout/img/react.svg]

--- src/actions/AuthenticateionAction.tsx
+++
@@ -1,8 +0,0 @@
-export const SHOW_LOGIN_DIALOG = "SHOW_LOGIN_DIALOG";
-
-// TODO unused action, for login button
-export function getShowLohginDialogAction() {
-    return {
-        type: SHOW_LOGIN_DIALOG,
-    };
-}
