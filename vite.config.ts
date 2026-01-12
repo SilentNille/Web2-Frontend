76b6872a7fbe7d237b014ad6af32c2aa25d71a1d
Philip Niclas <philipp.niclas@unitb.de>
2026-01-12 14:28:03 +0100

    Update Vite configuration to set allowedHosts for specific domains
    Files: [vite.config.ts]

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { host: "0.0.0.0", port: 3000 , allowedHosts: true },
+  server: { host: "0.0.0.0", port: 3000 , allowedHosts: ["phni9317-frontend.project.ris.bht-berlin.de"] },
 })
